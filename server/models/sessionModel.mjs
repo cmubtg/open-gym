98e3b467386f7727bba7187e35599641e54f1697
Tyrece Jeffrey <tyrece.jeffrey@gmail.com>
2024-01-02 16:37:50 -0500

    clean database style

--- server/models/sessionModel.mjs
+++ server/models/sessionModel.mjs
@@ -1,24 +1,25 @@
 import mongoose from 'mongoose';
 import writeToCSV from '../utils/writeToCSV.mjs';
 
-
 const sessionSchema = new mongoose.Schema({
     id: {type: Number, required: true},
     time: {type: Date, required: true},
 }, {timestamps: true});
 
 // Extend Session model with custom methods
-sessionSchema.statics.getAllData = async function() {
+sessionSchema.statics.getAllData = async () => {
     return await this.find({});
-  }
-sessionSchema.statics.deleteAllData = async function(){
+}
+
+sessionSchema.statics.deleteAllData = async () => {
     // *** Insert deletion code HERE ***
-  }
-sessionSchema.statics.moveAllData = async function () { 
+}
+
+sessionSchema.statics.moveAllData = async () => { 
     const data = await this.find({});
     writeToCSV(data);
     deleteAllData();
-  }
+}
 
 export default mongoose.model('Session', sessionSchema);
 
