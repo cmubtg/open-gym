3bc545128c4279d6b9c6d89710c2bd3dc4b41f51
Ire Alarape <ialarape@andrew.cmu.edu>
2024-01-02 18:13:42 -0600

    offcenter dot fix

--- frontend/src/components/LiveDot.jsx
+++ frontend/src/components/LiveDot.jsx
@@ -3,10 +3,15 @@ import React from 'react';
 const LiveDot = ({msg}) => {
     return (
         <div className="flex flex-row items-center mt-[-5px]">
-            <div className="w-5 h-8 flex items-center">
-                <div className={`w-[10px] h-[10px] bg-btg-red rounded-full flex items-center justify-center`}>
-                    <div className={`w-[15px] h-[15px] border-btg-red border-2 rounded-full absolute animate-pulsate`}></div>
+            <div className="w-5 h-8 relative">
+                <div className={`w-[10px] h-[10px] bg-btg-red rounded-full absolute
+                                top-[11px] left-[0px]`}>
                 </div>
+                <div className={`w-[18px] h-[18px] border-btg-red border-2 rounded-full 
+                                absolute top-[7px] left-[-4px] animate-pulsate`}>
+
+
+                        </div>
             </div>
             <p className="text-xs leading-[0px] font-light">
                 {msg}
