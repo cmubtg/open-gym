7aa33ec9832152d5645adb3bc111cabc16e81dd2
Ire Alarape <ialarape@andrew.cmu.edu>
2024-01-02 21:16:50 -0600

    Change Meter color based on occupancy

--- frontend/src/components/OccMeter.jsx
+++ frontend/src/components/OccMeter.jsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from 'react';
+import { getOccClass } from '../utils/utils';
 
 const OccMeter = () => {
     // TODO Fetch occupancy data from API
     var offset = Math.floor(Math.random() * 200) + 1;
     var perc = Math.max(0,Math.floor((((offset/200) * 100) - 90) * -1))
-
+    var occ_class = getOccClass(perc);
     return (
         <div className="meter_container">  
         {/* view box left, up/down, zoom in, zoom out */}
             <svg className="w-full h-full" viewBox="20 -10 90 70">
                 <circle id="progress_track" cx="65" cy="70" r="70"></circle>
                 <circle id="progress" cx="65" cy="70" r="70"
-                strokeDashoffset={offset}></circle>
+                strokeDashoffset={offset}
+                className={`${occ_class}`}></circle>
             </svg>
             <div className="meter_info">
                 <p className="font-bold">{perc}<span className="text-xs">%</span></p>
