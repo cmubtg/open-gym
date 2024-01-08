4f23f9a0a0acbffcc794daba890b9c705a3eab70
Tyrece Jeffrey <tyrece.jeffrey@gmail.com>
2024-01-08 03:09:37 -0500

    refactored csv data dump

--- server/server.mjs
+++ server/server.mjs
@@ -1,4 +1,3 @@
-/* eslint-disable no-undef */
 import dotenv from 'dotenv';
 dotenv.config(); // load env variables
 
@@ -22,17 +21,18 @@ app.use((req, res, next) => {
 app.use('/api/', OpenGymRoutes);
 
 // connect to database
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI) // eslint-disable-line no-undef
     .then(() => {
       console.log('Connected to database');
 
       // weekly clearing database
       cron.schedule('0 0 * * 0', async () => {
-        db.moveAllData();
+        db.gymMoveAllRecords();
       });
 
       // listen on port
-      app.listen(process.env.PORT, () => {
+      app.listen(process.env.PORT, () => { // eslint-disable-line no-undef
+        // eslint-disable-next-line no-undef
         console.log('Listening on port', process.env.PORT);
       });
     })
---
+++ server/utils/constants.mjs
@@ -0,0 +1,13 @@
+export const metadata = 'metadata';
+
+export const getCSVFileName = (name) => 'data/' + name + '_data.csv';
+
+export const dayOfTheWeek = [
+  'sunday',
+  'monday',
+  'tuesday',
+  'wednesday',
+  'thursday',
+  'friday',
+  'saturday',
+];
