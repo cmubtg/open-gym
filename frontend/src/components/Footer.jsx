f1b87071baa82105e32a2255be516dc81d0393ac
wenchaohu1 <118210513+wenchaohu1@users.noreply.github.com>
2024-01-10 16:54:14 -0500

    Add social media links to the footer. (generated by blackbox.ai ) (#35)

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import BTGLogo from './BTGLogo';
+
+import {  FiGithub } from "react-icons/fi";
+import { FaInstagram , FaLinkedin} from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="min-h-28 text-gray-500 bg-gray-50 dark:bg-btg-dark-grey p-8 text-sm
-                    w-full">
-      <p>&copy; 2023 OpenGym. All rights reserved.</p>
+    <footer className="min-h-28 text-white bg-btg-dark-grey p-8 text-sm w-full flex flex-row items-center
+                      justify-between">
+
+      <div className="w-42 h-full flex flex-row">
+        <BTGLogo/>
+        <div className="w-42 h-full flex flex-col ml-6 "> 
+          <p className="text-white">&copy; 2023 OpenGym</p> 
+          <p className="text-white">All rights reserved.</p>
+        </div>
+      </div>
+      
+      <div className="flex space-x-4 mr-6"> 
+        <a href="https://github.com/cmubtg" rel="noopener noreferrer"> 
+          <FiGithub size="20" className="nav_icon text-white" />
+        </a>
+
+        <a href="https://www.instagram.com/cmubtg" rel="noopener noreferrer"> 
+          <FaInstagram size="20" className="nav_icon text-white" />
+        </a>
+ 
+        <a href="https://www.linkedin.com/company/cmubtg" rel="noopener noreferrer"> 
+          <FaLinkedin size="20" className="nav_icon text-white" />
+        </a> 
+      </div>
+      
     </footer>
   );
 }
