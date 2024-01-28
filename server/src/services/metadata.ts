99bc8d25d5e7b897dca3b6a0f46397b0cb627004
Tyrece Jeffrey <tyrece.jeffrey@gmail.com>
2024-01-28 14:36:11 -0500

    returning default metadata

--- server/src/services/metadata.ts
+++ server/src/services/metadata.ts
@@ -1,29 +1,10 @@
 import * as metadata from '../../data/metadata.json';
-import { DAYS_OF_THE_WEEK, GYM_NAMES, gymNameType } from '../utils/constants';
-import db from '../models/database';
+import { gymNameType } from '../utils/constants';
 
-const appendMetadataHours = async (date: Date, gym: gymNameType) => {
-  const specialHours = await db.getNextWeekGymHours(gym, date);
-  specialHours.forEach((hours) => {
-    const { date, open, close } = hours;
-    const day = DAYS_OF_THE_WEEK[date.getDay()];
-    metadata[gym].hours[day] = {
-      open: open,
-      close: close
-    };
-  });
-};
-
-export const getAllMetadataHelper = async () => {
-  const currentDate = new Date();
-  for (const gym of GYM_NAMES) {
-    await appendMetadataHours(currentDate, gym);
-  }
+export const getAllMetadataHelper = () => {
   return metadata;
 };
 
-export const getGymMetadataHelper = async (gym: gymNameType) => {
-  const currentDate = new Date();
-  await appendMetadataHours(currentDate, gym);
+export const getGymMetadataHelper = (gym: gymNameType) => {
   return metadata[gym];
 };
