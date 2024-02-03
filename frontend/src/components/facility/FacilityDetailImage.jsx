cb0b714a38f566bce326b7be764e03e987a67f8d
Ire Alarape <67347046+Ireoluwa-A@users.noreply.github.com>
2024-02-03 16:14:02 -0500

    Facility Detail Images (#57)
    * added img on facility detail for tep and fairfax
    
    * updated weigand image
    
    ---------
    
    Co-authored-by: Nihitha <nsunil@andrew.cmu.edu>

--- frontend/src/components/facility/FacilityDetailImage.jsx
+++ frontend/src/components/facility/FacilityDetailImage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 const FacilityDetailImage = ({facility, isMobile}) => {
+  console.log("second facility", facility.image2);
     return (
       <>
         {isMobile ? (
           <div className="w-full h-52 gradient_overlay">
             <img className="w-full h-full object-cover brightness-50" src={facility.image2} alt={facility.name}/>
-          </div>
+          </div > 
         ) : (
           <div className="btg_container h-[200px] mt-4 flex flex-row">
             <img className="w-full h-full img_filter" src={facility.image2} alt={facility.name}/>
