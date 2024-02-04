fc4b7ad5bab925fafcf9af960b15d9ef40299ca7
Wenchao Hu <wenchaoh@andrew.cmu.edu>
2024-02-03 19:55:36 -0500

    Fixed padding on amenities

--- frontend/src/components/facility/FacilityDetailGeneralAmenities.jsx
+++ frontend/src/components/facility/FacilityDetailGeneralAmenities.jsx
@@ -1,17 +1,17 @@
 
 const FacilityDetailGeneralAmenities = ({facility}) => { 
   
-  return (
-    
-    <div className="btg_container h-[400px] flex flex-col justify-center">
+  return ( 
+    <div className="btg_container h-[500px] flex flex-col justify-center">
+
       <h2 className="font-semibold mb-4">General Amenities</h2> 
  
-      <div className="amenities w-full overflow-auto pt-0">
+      <div className="amenities w-full pt-0">
      
           <div className="amenities_card"> 
             <div className="h-full">
                 <img className= {`card_img`} src={facility.image3} alt={facility.name} /> 
-                <p className="p-2"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                <p className="p-2"> Lorem ipsum dolor sit amet, consectetur adipiscing elit.  </p>
             </div>
           </div>  
 
