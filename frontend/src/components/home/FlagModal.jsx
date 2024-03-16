7f7c1c6131c5f92cfe668489ae5b66c14ffddb0c
Wenchao Hu <wenchaoh@andrew.cmu.edu>
2024-03-16 15:31:56 -0400

    Make flag clickable & changed pointer

--- frontend/src/components/home/FlagModal.jsx
+++ frontend/src/components/home/FlagModal.jsx
@@ -5,18 +5,23 @@ import { FaFlag } from "react-icons/fa";
 const FlagModal = () => {
     const [showModal, setShowModal] = useState(false);
 
+    const toggleModal = () => {
+      setShowModal(!showModal);
+    };
+
     return (
-      <div onMouseEnter={() => setShowModal(true)} onMouseLeave={(e) => !e.relatedTarget || !e.relatedTarget.classList.contains('modal-overlay') ? setShowModal(false) : null}>
+      <div onClick={toggleModal}>
         <FaFlag className="flex ml-2.5 mt-1.5 flag_icon"/>
         <div className="rectangle">{showModal && <div className="triangle"></div>}</div>
-        {showModal && (
-          <div className="modal-overlay">
-            <h2>Spring Break Hours</h2>
-            <p>Monday - Friday: 6:30am - 11:30pm</p>
-            <p className="mt-0.5">Saturday & Sunday: 9:00am - 10:00pm</p>
-          </div>
-        )}
+            {showModal && (
+                <div className="modal-overlay">
+                    <h2>Spring Break Hours</h2>
+                    <p>Monday - Friday: 6:30am - 11:30pm</p>
+                    <p className="mt-0.5">Saturday & Sunday: 9:00am - 10:00pm</p>
+                </div>
+            )}
       </div>
+ 
     );
 }
 
