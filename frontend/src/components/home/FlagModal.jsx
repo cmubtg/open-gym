fce1c28219965c677e2205b0a6793e4e4d16c175
Wenchao Hu <wenchaoh@andrew.cmu.edu>
2024-03-29 12:15:41 -0400

    Completed click outside to close, debugged

--- frontend/src/components/home/FlagModal.jsx
+++ frontend/src/components/home/FlagModal.jsx
@@ -1,37 +1,53 @@
-import React from 'react';
-import { useState, useRef, useEffect} from 'react';
+import * as React from 'react';
 import { FaFlag } from "react-icons/fa"; 
-
-const FlagModal = () => { 
  
-    const [open, setOpen] = useState(false);
-    const modalRef = useRef(null);
+const FlagModal = () => { 
+  const [showModal, setShowModal] = React.useState(false);
 
-    useEffect(() => {
-      const handleClickOutside = (event) => {
-        if (modalRef.current && !modalRef.current.contains(event.target)) {
-          setOpen(false); 
-        }
-      };
+  const handleClickOutside = () => {
+    setShowModal(false);
+  };
 
-      document.addEventListener("mousedown", handleClickOutside); 
-    }, []); 
-    
-    return (
-      <div className='flag' onClick={() => setOpen(!open)}> 
-          <FaFlag className="flex ml-2.5 mt-1.5 flag_icon"/>  
-          <div className="rectangle">
-              {open && <div className="triangle"></div>}
-              {open && (
-                  <div className="modal-overlay">
-                      <h2>Spring Break Hours</h2>
-                      <p>Monday - Friday: 6:30am - 11:30pm</p>
-                      <p className="mt-0.5">Saturday & Sunday: 9:00am - 10:00pm</p>
-                  </div>
-              )}
+  const ref = useOutsideClick(handleClickOutside);
+
+  const handleFlagClick = () => {
+    setShowModal(true);
+  };
+
+  return (
+    <div className='flag_icon'>  
+      <div ref={ref}>
+        {showModal && (
+          <div className="modal-overlay" ><div className="triangle"></div>
+            <h2>Spring Break Hours</h2>
+            <p>Monday - Friday: 6:30am - 11:30pm</p>
+            <p className="mt-0.5">Saturday & Sunday: 9:00am - 10:00pm</p>
           </div>
-      </div>
+        )}
+      </div> 
+      <FaFlag className="flex ml-2.5 mt-1.5 flag_icon" onClick={handleFlagClick} style={{ cursor: 'pointer' }} />
+    </div>
   );
-};
+} 
 
-export default FlagModal;
+export default FlagModal;;
+
+const useOutsideClick = (callback) => {
+  const ref = React.useRef();
+
+  React.useEffect(() => {
+    const handleClick = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        callback();
+      }
+    };
+
+    document.addEventListener('click', handleClick, true);
+
+    return () => {
+      document.removeEventListener('click', handleClick, true);
+    };
+  }, [ref]);
+
+  return ref;
+};
