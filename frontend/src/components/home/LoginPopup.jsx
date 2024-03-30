7a408b5645e76cae8aca693f441791a64ca19450
Wenchao Hu <wenchaoh@andrew.cmu.edu>
2024-03-30 16:31:23 -0400

    fixed wording on login popup

--- frontend/src/components/home/LoginPopup.jsx
+++ frontend/src/components/home/LoginPopup.jsx
@@ -9,9 +9,8 @@ const LoginPopup = ({ setShowLogin }) => {
     <div className="login-popup">
       <div className="login-popup-content">
         <h2>Open Gym</h2>
-        <p id="body">Open Gym is built and maintained by CMUBTG. To ensure the security 
-          of the student data, it is mandatory for you to use your Andrew ID.
-          <a href="https://cmubtg.com/">here</a>.</p>
+        <p id="body">Open Gym is built and maintained by <a href="https://cmubtg.com/">CMU BTG</a>. To ensure the security 
+          of the student data, it is mandatory for you to use your Andrew ID.</p>
         <p id="loggedOut"><strong>You are currently logged out. Log in to access.</strong></p>
         <button onClick={handleLogin}>Login</button>
       </div>
