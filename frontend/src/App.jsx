ac44ee612271cd3097c4efffd4c0f192f1078bf5
Tyrece <tyrece.jeffrey@gmail.com>
2024-11-01 17:56:34 -0400

    [WIP] Initial google oauth login
    Co-authored-by: Ire Alarape <Ireoluwa-A@users.noreply.github.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google'; // Import GoogleOAuthProvider
 import { Home, FacilityDetail } from './pages';
 import Footer from './components/footer/Footer';
 
 function App() {
   return (
+    <GoogleOAuthProvider clientId="935523924383-h3j9osncso40dml2iiltmq9kuc1nteh5.apps.googleusercontent.com">
       <>
         <Router>
           <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/facility/:id" element={<FacilityDetail/>} />
+            <Route path="/" element={<Home />} />
+            <Route path="/facility/:id" element={<FacilityDetail />} />
           </Routes>
-        </Router> 
-        <Footer/>
+        </Router>
+        <Footer />
       </>
+    </GoogleOAuthProvider>
   );
 }
 
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,13 +3,18 @@ import mongoose from 'mongoose';
 import OpenGymRoutes from './routes/routes';
 import config from './config';
 import { initJobs } from './jobs';
+import { OAuth2Client } from 'google-auth-library';
+
 const cors = require('cors');
 
+const CLIENT_ID = '935523924383-h3j9osncso40dml2iiltmq9kuc1nteh5.apps.googleusercontent.com';
+const client = new OAuth2Client(CLIENT_ID);
 
 const app = express();
 
 app.use(cors({
   origin: config.frontendURL,
+  credentials: true
 }));
 
 // middleware
@@ -22,6 +27,39 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/', OpenGymRoutes);
+
+// attempt to login
+app.post('/auth/google', async (req, res) => {
+  const { token } = req.body;
+
+  try {
+    // Verify the token with Google
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: CLIENT_ID,
+    });
+
+    // Get the user’s email
+
+    // @ts-ignore: Testing
+    const payload = ticket.getPayload();
+    // @ts-ignore: Testing
+    const email = payload.email;
+
+    // Check if the email domain matches the university domain
+    
+    // @ts-ignore: Testing
+    if (email.endsWith('@andrew.cmu.edu')) {
+      res.json({ success: true, message: 'Login successful' });
+    } else {
+      res.json({ success: false, message: 'Invalid email domain' });
+    }
+  } catch (error) {
+    console.error('Error verifying Google ID token:', error);
+    res.status(400).json({ success: false, message: 'Invalid token' });
+  }
+});
+
 
 // connect to database
 mongoose.connect(config.databaseURL)
