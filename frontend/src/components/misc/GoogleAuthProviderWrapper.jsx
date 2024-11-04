0be46974016eac65c8ed388c34b9b53686cb0b31
Ire Alarape <ialarape@andrew.cmu.edu>
2024-11-04 11:39:04 -0500

    Remove ClientID + refactor googleoauth provider
    github client id will be refreshed and included in .env file

---
+++ frontend/src/components/misc/GoogleAuthProviderWrapper.jsx
@@ -0,0 +1,13 @@
+// src/GoogleAuthProviderWrapper.js
+import React from "react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const GoogleAuthProviderWrapper = ({ children }) => {
+  return (
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+      {children}
+    </GoogleOAuthProvider>
+  );
+};
+
+export default GoogleAuthProviderWrapper;
