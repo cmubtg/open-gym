cb8d5ecf2f8e69d749524895fcf9e9d33b261ae5
Ire Alarape <ialarape@andrew.cmu.edu>
2025-01-03 23:04:22 -0600

    Update facilities.js

--- frontend/src/data/facilities.js
+++ frontend/src/data/facilities.js
@@ -16,78 +16,78 @@ const testHours = {
 
 const facilities = [
     {
-        id: "cohonFC",
-        name : "Cohon Fitness Center (Lower)",
-        address : "5000 Forbes Ave, Pittsburgh, PA 15213",
-        description : `Two floors dedicated to cardio and weight equipment in
-        the Cohon Univeristy Center.`,
-      
-       hours : Array.from({ length: 7 }, () => alwaysOpen), // 7 days a week standard hours
-       image: process.env.PUBLIC_URL + "../images/uc.jpg",
-       image2: process.env.PUBLIC_URL + "../images/uc2.jpg",
-       image3: process.env.PUBLIC_URL + "../images/uc3.jpg",
-       max_occupancy: 150,
-       status: "established"
+      id: "cohonFC",
+      name : "Cohon Fitness Center (Lower)",
+      address : "5000 Forbes Ave, Pittsburgh, PA 15213",
+      description : `Two floors dedicated to cardio and weight equipment in
+      the Cohon Univeristy Center.`,
+    
+      hours : Array.from({ length: 7 }, () => alwaysOpen), // 7 days a week standard hours
+      image: process.env.PUBLIC_URL + "../images/uc.jpg",
+      image2: process.env.PUBLIC_URL + "../images/uc2.jpg",
+      image3: process.env.PUBLIC_URL + "../images/uc3.jpg",
+      max_occupancy: 150,
+      status: "established"
     }, 
     {
-        id: "cohon2FC",
-        name : "Cohon Fitness Center (Upper)",
-        address : "5000 Forbes Ave, Pittsburgh, PA 15213",
-        description : `Two floors dedicated to cardio and weight equipment in
-        the Cohon Univeristy Center.`,
-      
-       hours : Array.from({ length: 7 }, () => alwaysOpen), // 7 days a week standard hours
-       image: process.env.PUBLIC_URL + "../images/ucA1.jpg",
-       image2: process.env.PUBLIC_URL + "../images/uc2.jpg",
-       image3: process.env.PUBLIC_URL + "../images/uc3.jpg",
-       max_occupancy: 150,
-       status: "coming soon"
+      id: "cohon2FC",
+      name : "Cohon Fitness Center (Upper)",
+      address : "5000 Forbes Ave, Pittsburgh, PA 15213",
+      description : `Two floors dedicated to cardio and weight equipment in
+      the Cohon Univeristy Center.`,
+    
+      hours : Array.from({ length: 7 }, () => alwaysOpen), // 7 days a week standard hours
+      image: process.env.PUBLIC_URL + "../images/ucA1.jpg",
+      image2: process.env.PUBLIC_URL + "../images/uc2.jpg",
+      image3: process.env.PUBLIC_URL + "../images/uc3.jpg",
+      max_occupancy: 150,
+      status: "coming soon"
     }, 
 
     {
-    id : "tepperFC",
-        name : "Tepper Fitness Center",
-        address : "",
-        description : ``,
-        hours : Array.from({ length: 7 }, () => alwaysOpen), 
-        image: process.env.PUBLIC_URL + "./images/tepper.jpg",
-        image2: process.env.PUBLIC_URL + "../images/tepper2.jpg",
-        max_occupancy: 70,
-        status: "coming soon"
+      id : "tepperFC",
+      name : "Tepper Fitness Center",
+      address : "",
+      description : ``,
+      hours : Array.from({ length: 7 }, () => alwaysOpen), 
+      image: process.env.PUBLIC_URL + "./images/tepper.jpg",
+      image2: process.env.PUBLIC_URL + "../images/tepper2.jpg",
+      max_occupancy: 70,
+      status: "coming soon"
     }, 
     // {
-    //     id : "fairfax",
-    //     name : "Fairfax Gym",
-    //     address : "",
-    //     description : ``,
-    //     hours : Array.from({ length: 7 }, () => testHours), 
-    //     image: process.env.PUBLIC_URL + "./images/uc.jpg",
-    //     image2: process.env.PUBLIC_URL + "../images/fairfax2.jpg",
-    //     max_occupancy: 30,
-    //     status: "coming soon"
+    //   id : "fairfax",
+    //   name : "Fairfax Gym",
+    //   address : "",
+    //   description : ``,
+    //   hours : Array.from({ length: 7 }, () => testHours), 
+    //   image: process.env.PUBLIC_URL + "./images/uc.jpg",
+    //   image2: process.env.PUBLIC_URL + "../images/fairfax2.jpg",
+    //   max_occupancy: 30,
+    //   status: "coming soon"
     // },
     {
-        id : "wiegand",
-        name : "Wiegand Gym",
-        address : "",
-        description : ``,
-        hours : Array.from({ length: 7 }, () => standardHours), 
-        image: process.env.PUBLIC_URL + "./images/wiegand.jpg",
-        image2: process.env.PUBLIC_URL + "../images/wiegand.jpg",
-        max_occupancy: 300,
-        status: "coming soon"
+      id : "wiegand",
+      name : "Wiegand Gym",
+      address : "",
+      description : ``,
+      hours : Array.from({ length: 7 }, () => standardHours), 
+      image: process.env.PUBLIC_URL + "./images/wiegand.jpg",
+      image2: process.env.PUBLIC_URL + "../images/wiegand.jpg",
+      max_occupancy: 300,
+      status: "coming soon"
     },
 
     {
-        id : "highmark",
-        name : "Highmark Gym",
-        address : "",
-        description : ``,
-        hours : Array.from({ length: 7 }, () => standardHours), 
-        image: process.env.PUBLIC_URL + "./images/highmark.jpg",
-        image2: process.env.PUBLIC_URL + "../images/highmark.jpg",
-        max_occupancy: 1000,
-        status: "coming soon"
+      id : "highmark",
+      name : "Highmark Gym",
+      address : "",
+      description : ``,
+      hours : Array.from({ length: 7 }, () => standardHours), 
+      image: process.env.PUBLIC_URL + "./images/highmark.jpg",
+      image2: process.env.PUBLIC_URL + "../images/highmark.jpg",
+      max_occupancy: 1000,
+      status: "coming soon"
     }
 ];
 
