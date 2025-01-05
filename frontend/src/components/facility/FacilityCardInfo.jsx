95ee6f3cb336160f3f46b9f9615dd6a3f943420f
Ire Alarape <ialarape@andrew.cmu.edu>
2025-01-05 17:58:04 -0600

    Skeleton occupancy display

--- frontend/src/components/facility/FacilityCardInfo.jsx
+++ frontend/src/components/facility/FacilityCardInfo.jsx
@@ -7,21 +7,58 @@ import { useAuth } from "context/AuthContext";
 const FacilityCardInfo = () => {
   const { facility, closingStatus } = useFacility();
   const isComingSoon = facility.status === "coming soon";
-  const { isAuthenticated } = useAuth()
+  const { isAuthenticated } = useAuth();
 
-  const showOccInfo = !isComingSoon && !isClosed(closingStatus) && isAuthenticated; 
-  // const showOccInfo = true
+  const showOccInfo = !isComingSoon && !isClosed(closingStatus);
+
   return (
     <div className={`card_btm ${(isClosed(closingStatus) || isComingSoon) && "opacity-55"}`}>
-      
       {/* Facility Name */}
       <div className="w-[75%] h-full mt-2.5 min-[340px]:mt-4 flex flex-col">
         <h3>{facility.name}</h3>
-        {showOccInfo && <LiveDot />}
+        {showOccInfo && (
+          isAuthenticated ? (
+            <LiveDot />
+          ) : (
+            <div className="flex items-center gap-2 mt-3">
+              <div className="w-40 h-3 bg-btg-light-grey dark:bg-btg-dark-grey rounded" />
+            </div>
+          )
+        )}
       </div>
 
       {/* Occupancy Meter */}
-      {showOccInfo && <FacilityCardMeter />}
+      {showOccInfo && (
+        isAuthenticated ? (
+          <FacilityCardMeter />
+        ) : (
+          <div className="min-w-[150px] h-full mt-4">
+            <SkeletonMeter />
+          </div>
+        )
+      )}
+    </div>
+  );
+};
+
+const SkeletonMeter = () => {
+  return (
+    <div className="meter_container">
+      <svg className="w-full h-full" viewBox="0 -10 90 70">
+        <circle 
+          id="meter_track" 
+          cx="65" 
+          cy="70" 
+          r="70"
+          className="stroke-btg-light-grey dark:stroke-btg-dark-grey"
+        ></circle>
+      </svg>
+      <div className="meter_info">
+        <div className="w-8 h-4 bg-btg-light-grey dark:bg-btg-dark-grey rounded" />
+        <p className="font-light text-[7px] mt-1 text-btg-light-grey dark:text-btg-dark-grey">
+          of max occupancy
+        </p>
+      </div>
     </div>
   );
 };
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -17,13 +17,16 @@ const Home = () => {
 };
 
 const TitleBar = () => {
+  const { isAuthenticated } = useAuth();
   return (
     <div className="w-full flex flex-row justify-between items-center">
       <div>
         <h1>Facility Occupancy</h1>
-        {/* TODO: n/BPS-222 - Add facility details description. */}
-        {/* <p className="">Click on a gym for more occupancy information!</p> */}
-        <p>Real-time gym occupancy information</p>
+        <p className={`${!isAuthenticated ? 'text-red-500' : ''}`}>
+          {isAuthenticated 
+            ? "Real-time gym occupancy information" 
+            : "Sign in to view real-time occupancy information"}
+        </p>
       </div>
       <div className="flex items-center gap-1 mt-14">
         <ThemeIcon />
