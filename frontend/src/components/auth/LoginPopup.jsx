4ae9ff4d4ab6c369107f644755993bc1c09881a3
phdoshi <phdoshi@andrew.cmu.edu>
2025-01-23 22:50:42 -0500

    fix login dark mode + sizing

--- frontend/src/components/auth/LoginPopup.jsx
+++ frontend/src/components/auth/LoginPopup.jsx
@@ -14,14 +14,14 @@ const LoginPopup = ({ setShowLogin }) => {
           <a href="https://cmubtg.com/">CMU BTG</a>.<span className="font-medium text-btg-red">To view real-time occupancy
           data, please sign in with a CMU email </span>
         </p>
-        <div className="flex flex-col gap-5 justify-between items-center mt-6  md:flex-row">
-          <div className="w-1/2 md:w-[45%]">
+        <div className="flex flex-col sm:flex-row gap-5 justify-between items-center mt-6 ">
+          <div className="w-[14em] md:w-[45%]">
             <GoogleLoginButton />
           </div>
-          <div className="w-1/2 md:w-[45%]">
+          <div className="w-[14em] md:w-[45%]">
             <p
               onClick={continueAsGuest}
-              className="cursor-pointer flex items-center justify-center h-10 border border-gray-300 rounded-sm w-full duration-300 hover:text-btg-red hover:border-btg-red"
+              className="cursor-pointer flex items-center justify-center h-10 border dark:text-btg-med-grey border-gray-300 rounded-sm w-full duration-300 hover:text-btg-red hover:border-btg-red"
             >
               continue without signing in
             </p>
