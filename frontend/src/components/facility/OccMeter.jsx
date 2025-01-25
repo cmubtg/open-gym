b55db95ea491285bb382c08688c83fd1422e39d6
phdoshi <phdoshi@andrew.cmu.edu>
2025-01-25 15:29:21 -0500

    simplify occupancy calc

--- frontend/src/components/facility/OccMeter.jsx
+++ frontend/src/components/facility/OccMeter.jsx
@@ -4,11 +4,8 @@ import { getFacilitiesMetadata } from "data/facilities";
 
 const OccMeter = () => {
   const { occupancy, facility } = useFacility();
-  const metadata = getFacilitiesMetadata()
-  // want to filter metadata list by id that matches facility name
-  const facilityMetadata = metadata.find((item) => item.id === facility.id);
-  const maxOccupancy = facilityMetadata?.occupancy
-  
+  const maxOccupancy = facility?.max_occupancy
+
   // Calculate the strokeDashoffset value for the meter_level circle
   const calculateOffset = (percentage) => {
     const boundedPerc = Math.max(0, Math.min(100, percentage));
@@ -16,7 +13,7 @@ const OccMeter = () => {
     const endOffset = -20; // offset at 100%
     return startOffset + (endOffset - startOffset) * (boundedPerc / 100);
   };
-  const occupancyPercentage = (occupancy / maxOccupancy) * 100
+  const occupancyPercentage = ((occupancy / maxOccupancy) * 100).toFixed()
   const offset = calculateOffset(occupancyPercentage);
   const occ_class = getOccClass(occupancyPercentage);
 
