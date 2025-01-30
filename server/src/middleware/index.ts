06922cb8c101b091ae6fe63d9e97c676a4c67702
Ire Alarape <ialarape@andrew.cmu.edu>
2025-01-30 18:24:50 -0500

    Fix log validation bug + print headers

--- server/src/middleware/index.ts
+++ server/src/middleware/index.ts
@@ -8,6 +8,7 @@ const mountMiddleware = (app: Application): void => {
   // Request logging middleware.
   app.use((req: Request, res: Response, next: NextFunction) => {
     console.log(`${req.method} ${req.path}`);
+    console.log("Headers:", req.headers);
     next();
   });
 
