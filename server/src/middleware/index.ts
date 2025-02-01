977c658f567a1fdd343d2cf5c03cb6c4dbc66bc1
James Tcheng <james.tcheng1234@gmail.com>
2025-02-01 15:07:11 -0500

    Added logging error

--- server/src/middleware/index.ts
+++ server/src/middleware/index.ts
@@ -10,7 +10,7 @@ const mountMiddleware = (app: Application): void => {
   // Request logging middleware
   class MyStream {
     write(text: string) {
-        logger.info(text);
+        logger.info(text.replace(/\n$/, '')); // Disable Morgan adding \n to end of stream
     }
   }
   let myStream = new MyStream()
@@ -30,9 +30,11 @@ const mountMiddleware = (app: Application): void => {
   
     return `\x1b[${color}m${status}\x1b[0m`;
   });
+
+  const morganFormat = `\x1b[36m[:date[clf]]\x1b[0m \x1b[36m:method\x1b[0m \x1b[36m:url\x1b[0m :statusColor \x1b[35m:response-time ms\x1b[0m | \x1b[30m:user-agent\x1b[0m`
       
   app.use(
-      morgan(`\x1b[36m[:date[clf]]\x1b[0m \x1b[36m:method\x1b[0m \x1b[36m:url\x1b[0m :statusColor \x1b[35m:response-time ms\x1b[0m | \x1b[30m:user-agent\x1b[0m`, {stream: myStream})
+      morgan(morganFormat, {stream: myStream})
     );
 
 
