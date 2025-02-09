eeeaa3c639abb590d80a7b5421c4651e336f8288
Ire Alarape <ialarape@andrew.cmu.edu>
2025-02-08 22:05:29 -0500

    Remove decryption

--- server/src/middleware/hmac/hmac-controller.ts
+++ server/src/middleware/hmac/hmac-controller.ts
@@ -1,7 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { HttpStatus } from "@/utils";
 import { validateRequest } from "./validate-request";
-import { decrypt } from "./decrypt";
 
 // Authentication & Confidentiality: Ensures that the request is from a valid source then
 // decrypts the data to be processed and stored.
@@ -20,15 +19,8 @@ export const hmacAuth = (req: Request, res: Response, next: NextFunction) => {
       return;
     }
 
-    // Decrypt
-    try {
-      // req.body.direction = decrypt(encryptedData);
-      next();
-      return;
-    } catch (error) {
-      res.status(HttpStatus.BadRequest).json({ error: "Invalid encrypted data" });
-      return;
-    }
+    next();
+
   } catch (error) {
     res.status(HttpStatus.InternalServerError).json({ error: "Authentication failed" });
     return;
