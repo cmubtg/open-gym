726b3a2fa412a8c889eebed09a418ba3dff6a768
Ire Alarape <ialarape@andrew.cmu.edu>
2025-02-23 20:21:35 -0500

    Updated styling

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -22,14 +22,14 @@ const TitleBar = () => {
     <div className="w-full flex flex-col sm:flex-row justify-between items-center text-center sm:text-left">
       <div>
         <div className="flex justify-center items-center sm:justify-between">
-          <img className="block dark:hidden w-1/2 mb-2" src={process.env.PUBLIC_URL + "../images/light_mode_logo.png"} alt="Light Mode Logo" />
-          <img className="hidden dark:block w-1/2 mb-2" src={process.env.PUBLIC_URL + "../images/dark_mode_logo.png"} alt="Dark Mode Logo" />
+          <img className="block dark:hidden w-1/2 sm:w-1/4 mb-2" src={process.env.PUBLIC_URL + "../images/light_mode_logo.png"} alt="Light Mode Logo" />
+          <img className="hidden dark:block w-1/2 sm:w-1/4 mb-2" src={process.env.PUBLIC_URL + "../images/dark_mode_logo.png"} alt="Dark Mode Logo" />
         </div>
         {/* TODO: n/BPS-222 - Add facility details description. */}
         {/* <p className="">Click on a gym for more occupancy information!</p> */}
         <p>Real-time gym occupancy information</p>
       </div>
-      <div className="flex items-center sm:gap-1 mt-4 sm:mt-14 text-center sm:text-left">
+      <div className="flex items-center sm:gap-1 mt-6 sm:mt-9 text-center sm:text-left">
         <ThemeIcon />
         <AuthButton />
       </div>
