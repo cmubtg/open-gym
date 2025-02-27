160b82378924eaed2c44dac593c42b5c3f9c0bda
Ire Alarape <ialarape@andrew.cmu.edu>
2025-02-27 02:17:20 -0500

    [Temp] Ignore readings before 6:30

--- server/src/jobs/log-scan.ts
+++ server/src/jobs/log-scan.ts
@@ -28,19 +28,33 @@ export default function StartLogScanScheduler() {
 export const logScanJob = async () => {
   let currentTime = timeRoundedToNearestMinute(new Date());
   const occupancyRecords: OccupancyRecordType[] = [];
+
+  // Ignore all log records before 6:30am
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  const startTime = new Date(today);
+  startTime.setHours(6, 30, 0, 0);
+
+  const endTime = new Date(today);
+  endTime.setHours(23, 59, 59, 999);
+
   for (const gymName of GYM_NAMES) {
     const records = await db.getLogRecords({
       gym: gymName,
+      dateRange: {
+        start: startTime,
+        end: endTime,
+      }
     });
 
     const occupancy = records.reduce(
-      (acc, record) => acc + (record.entries - record.exits),
-      0
+        (acc, record) => acc + (record.entries - record.exits),
+        0
     );
     if (occupancy < 0) {
       console.log(
-        `Negative occupancy (${occupancy}) recorded for ${gymName} at ${currentTime}`,
-        records
+          `Negative occupancy (${occupancy}) recorded for ${gymName} at ${currentTime}`,
+          records
       );
       continue;
     }
@@ -51,14 +65,14 @@ export const logScanJob = async () => {
     });
   }
   await db.insertOccupancyRecords(
-    occupancyRecords,
-    OccupancyCollection.Current
+      occupancyRecords,
+      OccupancyCollection.Current
   );
   currentTime = timeRoundedToNearestMinute(new Date());
   console.log(`Log scan complete ${currentTime}`);
   for (const record of occupancyRecords) {
     console.log(
-      `Inserted occupancy record for ${record.gym} at ${record.time} with occupancy ${record.occupancy}`
+        `Inserted occupancy record for ${record.gym} at ${record.time} with occupancy ${record.occupancy}`
     );
   }
 };
