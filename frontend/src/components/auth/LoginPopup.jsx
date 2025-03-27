e5de6e4fa08481f8833e3cbfce21a3b8883584e5
Ire Alarape <ialarape@andrew.cmu.edu>
2025-03-27 01:01:20 -0400

    Sensor Bolt Maintenance

--- frontend/src/components/auth/LoginPopup.jsx
+++ frontend/src/components/auth/LoginPopup.jsx
@@ -10,29 +10,33 @@ const LoginPopup = ({ setShowLogin }) => {
       <div className="login-popup-content">
         <h2>Sign in</h2>
         <p id="body">
-          {"Thanks for using OpenGym! "}
-          <span className="font-medium text-btg-red">
-            Note that this project is in active development and the real-time
-            data we are providing are estimates.
-          </span>
-          <br />
-          <br />
-          If you have any feedback or suggestions, please reach out to us at
-          <a href="mailto:cmubtg@gmail.com" className="font-medium">
-            cmubtg@gmail.com
-          </a>
-          <br />
-          <br />
-          <span className="text-btg-red">
-            To view real-time occupancy estimates, please sign in with a valid
-            CMU email.
-          </span>
+          OpenGym is down for maintenance.{" "}
+          <span className={"font-medium text-btg-red"}>
+             Please check back 28th March 2025
+          </span>.
+          {/*{"Thanks for using OpenGym! "}*/}
+          {/*<span className="font-medium text-btg-red">*/}
+          {/*  Note that this project is in active development and the real-time*/}
+          {/*  data we are providing are estimates.*/}
+          {/*</span>*/}
+          {/*<br />*/}
+          {/*<br />*/}
+          {/*If you have any feedback or suggestions, please reach out to us at*/}
+          {/*<a href="mailto:cmubtg@gmail.com" className="font-medium">*/}
+          {/*  cmubtg@gmail.com*/}
+          {/*</a>*/}
+          {/*<br />*/}
+          {/*<br />*/}
+          {/*<span className="text-btg-red">*/}
+          {/*  To view real-time occupancy estimates, please sign in with a valid*/}
+          {/*  CMU email.*/}
+          {/*</span>*/}
         </p>
 
         <div className="flex flex-col sm:flex-row gap-5 justify-between items-center mt-6 pb-4">
-          <div className="w-[14em] md:w-[45%]">
-            <GoogleLoginButton />
-          </div>
+          {/*<div className="w-[14em] md:w-[45%]">*/}
+          {/*  <GoogleLoginButton />*/}
+          {/*</div>*/}
           <div className="w-[14em] md:w-[45%]">
             <p
               onClick={continueAsGuest}
