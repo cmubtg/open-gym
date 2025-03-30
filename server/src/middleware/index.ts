94a0735199a3b0c0a334bf36411bfc7b9e9bd361
Ire Alarape <ialarape@andrew.cmu.edu>
2025-03-29 20:43:56 -0400

    Use default morgan color status

--- server/src/middleware/index.ts
+++ server/src/middleware/index.ts
@@ -15,25 +15,10 @@ const mountMiddleware = (app: Application): void => {
       logger.info(text.replace(/\n$/, "")); // Disable Morgan adding \n to end of stream
     }
   }
-  let myStream = new MyStream();
+  const myStream = new MyStream();
 
-  // morgan.token('statusColor', (req, res): string => {
-  //   // Evaluate whether the response has been sent
-  //   const status: number | undefined = res.headersSent ? res.statusCode : undefined;
-
-  //   // Determine the ANSI color code based on the HTTP status
-  //   const color: number = status !== undefined
-  //     ? (status >= 500 ? 31 // red
-  //       : status >= 400 ? 33 // yellow
-  //       : status >= 300 ? 36 // cyan
-  //       : status >= 200 ? 32 // green
-  //       : 0)
-  //     : 0; // default to no color if status is undefined
-
-  //   return `\x1b[${color}m${status}\x1b[0m`;
-  // });
-
-  const morganFormat = `\x1b[36m[:date[clf]]\x1b[0m \x1b[36m:method\x1b[0m \x1b[36m:url\x1b[0m :statusColor \x1b[35m:response-time ms\x1b[0m | \x1b[30m:user-agent\x1b[0m`;
+  // Define a format string using the built-in :status token
+  const morganFormat = `\x1b[36m[:date[clf]]\x1b[0m \x1b[36m:method\x1b[0m \x1b[36m:url\x1b[0m \x1b[0m:status \x1b[35m:response-time ms\x1b[0m | \x1b[30m:user-agent\x1b[0m`;
 
   // Use morgan for logging HTTP requests
   app.use(morgan(morganFormat, { stream: myStream }));
