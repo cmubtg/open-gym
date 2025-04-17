d2b5f6e4d3b78fe566826e3a378b354a1b02452c
Ire Alarape <ialarape@andrew.cmu.edu>
2025-04-17 13:16:10 -0400

    Reset back to default message sensors up

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { FacilityCard, ThemeIcon, LoginPopup, AuthButton } from "components";
 import { useState, useEffect } from "react";
 import { getFacilitiesMetadata } from "data/facilities";
 import { useAuth } from "context/AuthContext";
-import {getTopBarMessage} from "data/topbarmessages";
+import { getTopBarMessage } from "data/topbarmessages";
 import Banner from "components/misc/Banner";
 
 const Home = () => {
@@ -21,7 +21,7 @@ const Home = () => {
 
 const TitleBar = () => {
   const { isAuthenticated } = useAuth();
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     setMessage(getTopBarMessage(isAuthenticated));
@@ -34,33 +34,33 @@ const TitleBar = () => {
   }, [isAuthenticated]);
 
   return (
-      <div>
-        <Banner>
-          <ManualOccupancyPSA/>
-        </Banner>
-        <div className="w-full flex flex-col sm:flex-row justify-between items-center text-center sm:text-left">
-          <div>
-            <div className="flex justify-center items-center sm:justify-between">
-              <img
-                className="block dark:hidden w-1/2 sm:w-1/4 mb-2"
-                src={process.env.PUBLIC_URL + "../images/light_mode_logo.png"}
-                alt="Light Mode Logo"
-              />
-              <img
-                className="hidden dark:block w-1/2 sm:w-1/4 mb-2"
-                src={process.env.PUBLIC_URL + "../images/dark_mode_logo.png"}
-                alt="Dark Mode Logo"
-              />
-            </div>
-            <p>{message}</p>
+    <div>
+      <Banner>
+        <ManualOccupancyPSA />
+      </Banner>
+      <div className="w-full flex flex-col sm:flex-row justify-between items-center text-center sm:text-left">
+        <div>
+          <div className="flex justify-center items-center sm:justify-between">
+            <img
+              className="block dark:hidden w-1/2 sm:w-1/4 mb-2"
+              src={process.env.PUBLIC_URL + "../images/light_mode_logo.png"}
+              alt="Light Mode Logo"
+            />
+            <img
+              className="hidden dark:block w-1/2 sm:w-1/4 mb-2"
+              src={process.env.PUBLIC_URL + "../images/dark_mode_logo.png"}
+              alt="Dark Mode Logo"
+            />
           </div>
-          <div className="flex items-center sm:gap-1 mt-6 sm:mt-9 text-center sm:text-left">
-            <ThemeIcon />
-            <AuthButton />
-          </div>
+          <p>{message}</p>
         </div>
-        <hr className="my-5"/>
+        <div className="flex items-center sm:gap-1 mt-6 sm:mt-9 text-center sm:text-left">
+          <ThemeIcon />
+          <AuthButton />
+        </div>
       </div>
+      <hr className="my-5" />
+    </div>
   );
 };
 
@@ -75,23 +75,23 @@ const FacilityCards = () => {
   );
 };
 
-const ManualOccupancyPSA = () =>{
-    const defaultMsg = "OpenGym is still under development. The numbers we provide are estimates and may be incorrect."
-    const sensorDownMsg = "OpenGym Sensors are currently down! The data being displayed may be inaccurate, please check back later."
+const ManualOccupancyPSA = () => {
+  const defaultMsg =
+    "OpenGym is still under development. The numbers we provide are estimates and may be incorrect.";
+  const sensorDownMsg =
+    "OpenGym Sensors are currently down! The data being displayed may be inaccurate, please check back later.";
   return (
     <div className="flex items-center gap-2">
-      <span className="py-2 font-bold">
-        {sensorDownMsg}
-      </span>
-    {/*TODO: n/BPS- Backend support for manual occupancy collecting*/}
-    {/*<strong>Are you in the gym right now?</strong> {" "} Help us refine our data by sharing your estimate of the current occupancy:*/}
-    {/*  <input*/}
-    {/*    type="number"*/}
-    {/*    placeholder="%"*/}
-    {/*    className="border border-[#ffc700] rounded px-2 py-1 w-16"*/}
-    {/*  />*/}
+      <span className="py-2 font-bold">{defaultMsg}</span>
+      {/*TODO: n/BPS- Backend support for manual occupancy collecting*/}
+      {/*<strong>Are you in the gym right now?</strong> {" "} Help us refine our data by sharing your estimate of the current occupancy:*/}
+      {/*  <input*/}
+      {/*    type="number"*/}
+      {/*    placeholder="%"*/}
+      {/*    className="border border-[#ffc700] rounded px-2 py-1 w-16"*/}
+      {/*  />*/}
     </div>
-  )
-}
+  );
+};
 
 export default Home;
